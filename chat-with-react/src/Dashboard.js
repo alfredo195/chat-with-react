e10ee7a71e92074c1d639f647af20fc3f93cdb89
BlackAce <alfredo.gon195@gmail.com>
2019-11-02 23:59:50 -0700

    added chip and topics

--- chat-with-react/src/Dashboard.js
+++ chat-with-react/src/Dashboard.js
@@ -2,7 +2,10 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import { height } from "@material-ui/system";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Chip from "@material-ui/core/Chip";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -10,7 +13,8 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 2)
   },
   flex: {
-    display: "flex"
+    display: "flex",
+    alignItems: "center"
   },
   topicsWindow: {
     width: "30%",
@@ -19,7 +23,8 @@ const useStyles = makeStyles(theme => ({
   },
   chatWindow: {
     width: "70%",
-    height: "300px"
+    height: "300px",
+    padding: "20px"
   },
   chatBox: {
     width: "85%"
@@ -40,13 +45,26 @@ export default function Dashboard() {
         </Typography>
         <Typography component="h5">topic</Typography>
         <div className={classes.flex}>
-          <div className={classes.topicsWindow}></div>
-          <div className={classes.chatWindow}></div>
+          <div className={classes.topicsWindow}>
+            <List>
+              {["topic"].map(topic => (
+                <ListItem key={topic} button>
+                  <ListItemText primary={topic} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+          <div className={classes.chatWindow}>
+            {[{ from: "user", msg: "hello" }].map((chat, i) => (
+              <div className={classes.flex} key = {i}>
+                <Chip label={chat.from} />
+                <Typography variant= "p">{chat.msg}</Typography>
+              </div>
+            ))}
+          </div>
         </div>
 
-        <div className={classes.flex}>
-
-        </div>
+        <div className={classes.flex}></div>
       </Paper>
     </div>
   );
